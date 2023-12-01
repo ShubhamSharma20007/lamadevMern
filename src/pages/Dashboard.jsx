fe76cdc0878594956f95060ca2c44c92bc779c1e
Raju Sharma <rs1248560@gmail.com>
2023-12-01 09:13:34 +0530

    four message

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import Slider from '../components/Slider/Slider';
 import Productcart from '../components/Productcart/Productcart';
 import Categories from '../components/categories/Categories';
+import axios from 'axios'
+import { useEffect } from 'react';
 const Dashboard = () => {
- 
+const fetchData =async()=>{
+  const res = await axios.get(process.env.MY_API_TOKEN)
+  const data =await res.data;
+  console.log(data);
+}
+
+useEffect(()=>{
+  fetchData()
+})
   return (
     <div>
     <Slider></Slider>
